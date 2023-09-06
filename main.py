9c7462c3be331c474fa78b755e888d870541c636
acedev003 <acedeveloper003@gmail.com>
2023-09-06 13:25:49 +0530

    Fixed issue of having starting direction into body itself causing instant death

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import time
 import random
 
-import  curses
-import _curses
-
-from curses import wrapper as curses_wrap
+import curses
+from   curses import wrapper as curses_wrap
 
 MIN_SCRN_HEIGHT = 25
 MIN_SCRN_WIDTH  = 80
@@ -16,16 +14,16 @@ class Snake:
         heady = random.randint(length,world_height-1)
         
         if (direction == self.DIR_UP):
-            return [(heady-i,headx)for i in range(length)]
-        
-        if (direction == self.DIR_DWN):
             return [(heady+i,headx)for i in range(length)]
         
+        if (direction == self.DIR_DWN):
+            return [(heady-i,headx)for i in range(length)]
+        
         if (direction == self.DIR_LFT):
-            return [(heady,headx-i)for i in range(length)]
+            return [(heady,headx+i)for i in range(length)]
         
         if (direction == self.DIR_RGT):
-            return [(heady,headx+i)for i in range(length)]
+            return [(heady,headx-i)for i in range(length)]
     
         
     def __log(self,msg):
@@ -34,11 +32,14 @@ class Snake:
         file.close()
     
     
-    def __init__(self,world_height,world_width,length=15):
+    def __init__(self,food:'Food',world_height,world_width,length=3):
         self.DIR_UP  =  1
         self.DIR_RGT =  2
         self.DIR_DWN = -1
         self.DIR_LFT = -2
+        
+        self.limitx  = world_width
+        self.limity  = world_height
 
         self.alive     = True
         self.length    = length
@@ -46,10 +47,13 @@ class Snake:
                                         self.DIR_LFT,
                                         self.DIR_RGT,
                                         self.DIR_UP])
+        
+        self.__log(self.direction)
 
         self.body      = self.__generate_body(world_height,world_width,self.length,self.direction)
         
     def draw(self,screen: 'curses._CursesWindow'):
+        self.__log(self.body)
         for bone in self.body:
             screen.addch(bone[0],bone[1],'#')
     
@@ -75,11 +79,31 @@ class Snake:
             body.append(bone)
 
         self.body = body
-        self.__log(body)
             
     def am_i_alive(self):
-        pass
+        if self.body[0][0] < 0 or self.body[0][0] > self.limity-1 or self.body[0][1] < 0 or self.body[0][1] > self.limitx-1:
+            self.alive = False
+            return False
+        
+        if self.body[0] in self.body[1:]: 
+            self.__log(self.body[0])
+            self.alive = False
+            return False
+        
+        self.alive = True
+        return True
+
+class Food:
+    def __init__(self,world_height,world_width):
+        self.position = (random.randint(0,world_height-1),random.randint(0,world_width-1))
+        
+    def get_position(self):
+        return self.position
     
+    def draw(self,screen: 'curses._CursesWindow'):
+        screen.addch(self.position[0],self.position[1],'O')
+    
+        
 def main(screen: 'curses._CursesWindow'):
     running = True
     
@@ -97,14 +121,22 @@ def main(screen: 'curses._CursesWindow'):
     screen.nodelay(True)
     curses.curs_set(0)
     
-    snake = Snake(maxy,maxx)
+    food  = Food(maxy,maxx)
+    snake = Snake(food,maxy,maxx)
     
     while running:    
         screen.clear()
         key = screen.getch()
+        snake.am_i_alive()
+        
+        if not snake.alive:
+            running = False
+            continue
+        
         if key == ord('q') or key == ord('Q'):
             running = False
             continue
+        
         if key == curses.KEY_UP:
             snake.set_direction(snake.DIR_UP)
         elif key == curses.KEY_DOWN:
@@ -114,8 +146,10 @@ def main(screen: 'curses._CursesWindow'):
         elif key == curses.KEY_RIGHT:
             snake.set_direction(snake.DIR_RGT)
         
+        food.draw(screen)
         snake.draw(screen)
         snake.move()
+        
         screen.refresh()
         time.sleep(0.09)
 
